1d98be203c9b2b41368e67726591c26c0aa5ce11
Sudan <sudanrawat59@gmail.com>
2025-09-12 10:29:26 +0530

    / issue in production url resolved

--- jwellery_frontend/src/environments/environment.prod.ts
+++ jwellery_frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://jwellery-application.onrender.com/' // later
+  apiUrl: 'https://jwellery-application.onrender.com' // later
 };
